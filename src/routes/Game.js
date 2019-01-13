59ba767e2d54de89c3ce387006ae71a2e5a3d4fb
Hanyoung Jeong <8735132@gmail.com>
2019-01-13 13:15:31 +0900

    게임 시작전 안내문 애니메이션 fadeout 및 element 삭제

--- src/routes/Game.js
+++ src/routes/Game.js
@@ -1,44 +1,12 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
-	state = {
-		people: [ '한영', '지원', '정석', '1', '2', '3', '4' ],
-		jobs: [
-			{
-				code: 1,
-				jobName: 'MAFIA',
-				count: 2
-			},
-			{
-				code: 2,
-				jobName: 'POLICE',
-				count: 1
-			},
-			{
-				code: 3,
-				jobName: 'DOCTOR',
-				count: 1
-			},
-			{
-				code: 4,
-				jobName: 'CITIZEN',
-				count: 3
-			}
-		],
-		num: 7
-	};
+	state = {};
 	componentWillMount() {
-		// console.log(this.state);
-		// const { jobs } = this.state;
 		const { people, num, jobs } = this.props;
-		// while(true) {
 
 		let c_people = [ ...people ];
 
-		// console.log('random index : ', randomIndex);
-		// console.log('random item :', people[randomIndex]);
-
-		console.log('random setting...');
 		jobs.forEach((job) => {
 			let flag_count = 0;
 			while (job.count !== flag_count) {
@@ -57,35 +25,34 @@ class Game extends Component {
 					flag_count += 1;
 				}
 			}
-			// Array.from({ length: job.count }, (v, k) => k).forEach((value) => {
-			// 	const randomIndex = Math.floor(Math.random() * num);
-			// 	// value -> count: 1,2,3 ...
-			// 	if(typeof c_people[randomIndex] === "string") {
-
-			// 	}
-			// 	console.log([
-			// 		{
-			// 			name: c_people.splice(randomIndex, 1)[0],
-			// 			code: job.code,
-			// 			jobName: job.jobName
-			// 		},
-			// 		...c_people
-			// 	]);
-			// });
 		});
-		console.log(c_people);
+		// console.log(c_people);
 		this.setState({
 			people: c_people
 		});
 	}
+	componentDidMount() {
+		this.refs.title.addEventListener('animationend', (e) => {
+			this.refs.title.removeEventListener('animationend', () => console.log('remove'));
+			this.refs.title.remove();
+			console.log('transitionend!!!');
+		});
+
+		setTimeout(() => {
+			this.refs.title.classList.add('fadeOutDown');
+		}, 2000);
+
+		window.addEventListener('keypress', (e) => {
+			console.log(e.code);
+		});
+	}
 	render() {
-		// const { num, jobs } = this.props;
-		// console.log(num);
-		// console.log(jobs);
 		const { people } = this.state;
 		return (
 			<div>
-				<h1>이제 각 사람 마다의 역할이 정해집니다.</h1>
+				<h1 className="animated" ref="title">
+					이제 각 사람 마다의 역할이 정해집니다.
+				</h1>
 				{people.map((person, i) => {
 					return <div key={`person_${i}`}>{person.code}</div>;
 				})}
