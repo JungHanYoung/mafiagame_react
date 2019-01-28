b7f2cb05c6c3acd2a26b3ecbb9146533a24e7265
ChoiBU <cbw0916@gmail.com>
2019-01-28 23:22:34 +0900

    randomjobs 추가

--- src/utils/setPlayers.js
+++ src/utils/setPlayers.js
@@ -1,23 +1,28 @@
-export const setPlayers = (people, jobs) => {
+export const setPlayers = (people, jobs, randomJobs) => {
 	/*
 	 * people 배열과 jobs 배열을 받아 players 반환
 	 */
 	let players = [];
-	jobs = jobs.map((job) => ({ ...job }));
+	let tmpJobs = [];
+	tmpJobs = jobs.map((job) => ({ ...job }));
 
 	people.forEach((person, index) => {
 		let randomJobsIndex;
 		do {
-			randomJobsIndex = Math.floor(Math.random() * jobs.length);
-		} while (!jobs[randomJobsIndex].count);
+			randomJobsIndex = Math.floor(Math.random() * tmpJobs.length);
+		} while (!tmpJobs[randomJobsIndex].count);
 		players[index] = {
 			name: person,
-			code: jobs[randomJobsIndex].code,
-			jobName: jobs[randomJobsIndex].jobName
+			code: tmpJobs[randomJobsIndex].code,
+			jobName: tmpJobs[randomJobsIndex].jobName
 		};
-		jobs[randomJobsIndex].count -= 1;
-		if (!jobs[randomJobsIndex].count) {
-			jobs.splice(randomJobsIndex, 1);
+		tmpJobs[randomJobsIndex].count -= 1;
+		if (!tmpJobs[randomJobsIndex].count) {
+			tmpJobs.splice(randomJobsIndex, 1);
+		}
+		// 필수 직업 배치 후 나머지 랜덤직업으로 세팅
+		if(tmpJobs.length === 0 && randomJobs && randomJobs.length){
+			tmpJobs = randomJobs.map((job) => ({ ...job }));
 		}
 	});
 
