e8712fd101c379bdd89f264b7ae47ba5a4389954
ChoiBU <cbw0916@gmail.com>
2019-01-31 21:25:41 +0900

    fix setPlayers

--- src/utils/setPlayers.js
+++ src/utils/setPlayers.js
@@ -6,25 +6,35 @@ export const setPlayers = (people, jobs, randomJobs) => {
 	let tmpJobs = [];
 	tmpJobs = jobs.map((job) => ({ ...job }));
 
+	let hasRandomJobs = randomJobs.length ? true : false;
+
 	people.forEach((person, index) => {
 		let randomJobsIndex;
-		do {
-			randomJobsIndex = Math.floor(Math.random() * tmpJobs.length);
-		} while (!tmpJobs[randomJobsIndex].count);
+		// count가 0인 직업 배열에서 제거
+		tmpJobs = tmpJobs.filter((job) => (job.count));
+		// 직업 랜덤 인덱스
+		randomJobsIndex = Math.floor(Math.random() * tmpJobs.length);
+		// 랜덤 직업 설정
 		players[index] = {
 			name: person,
 			code: tmpJobs[randomJobsIndex].code,
 			jobName: tmpJobs[randomJobsIndex].jobName
 		};
+		// 설정된 직업 카운트 다운
 		tmpJobs[randomJobsIndex].count -= 1;
+		// 설정 된 직업 카운트 0일 경우
 		if (!tmpJobs[randomJobsIndex].count) {
-			tmpJobs.splice(randomJobsIndex, 1);
-		}
-		// 필수 직업 배치 후 나머지 랜덤직업으로 세팅
-		if(tmpJobs.length === 0 && randomJobs && randomJobs.length && index < people.length -1){
-			tmpJobs = randomJobs.map((job) => ({ ...job }));
+			// 필수 직업들 배치 모두 끝난 경우 나머지 랜덤직업으로 세팅
+			if(tmpJobs.length === 1 && hasRandomJobs){
+				tmpJobs = randomJobs.map((job) => ({ ...job }));
+				hasRandomJobs = false;
+			}else{
+				// 아직 필수 직업이 남은 경우 해당 직업만 배열에서 제거
+				tmpJobs.splice(randomJobsIndex, 1);
+			}
 		}
 	});
+	console.table(players);
 
 	return players;
 };
