315370ef7d486ed75571f81c65d9582094fe0b87
Hanyoung Jeong <8735132@gmail.com>
2019-02-05 18:19:38 +0900

    CheckRole: context -> component

--- src/routes/CheckRole.js
+++ src/routes/CheckRole.js
@@ -6,76 +6,75 @@ import { withRouter } from 'react-router-dom';
 // component
 import { useGame } from '../context/GameContext';
 import { JOB_NAME_OF_MAFIA } from '../contants/Job';
+import { setPlayers } from '../utils/setPlayers';
 
 class CheckRole extends React.Component {
-	state = {
-		showRole: false,
-		showIndex: 0
-	};
-	componentWillMount() {
-		this.props.setRolePeople();
+	constructor(props) {
+		super(props)
+
+		const { jobs, people, randomJobs } = props
+		const players = setPlayers(people, jobs, randomJobs)
+
+		this.state = {
+			players,
+			showRole: false,
+			showIndex: 0
+		}
 	}
 
 	handleShowRole = () => {
-		const { showRole, showIndex } = this.state;
-		const { players } = this.props;
-
-		showRole
-			? this.setState({
-				showRole: false
-			})
-			: this.setState({
-				showRole: true
-			});
-
-		showRole &&
-			players.length > showIndex + 1 &&
+		const { showRole, showIndex, players } = this.state;
+		if (showRole) {
+			if (players.length - 1 > showIndex) {
+				this.setState({
+					showRole: false,
+					showIndex: showIndex + 1
+				})
+			} else {
+				this.props.history.push('/game', { players });
+			}
+		} else {
 			this.setState({
-				showIndex: showIndex + 1
-			});
-
-		showRole && players.length === showIndex + 1 && this.props.history.push('/game');
+				showRole: true
+			})
+		}
 	};
 
 	render() {
-		const { showRole, showIndex } = this.state;
-		const { players, gameStart, people } = this.props;
+		const { showRole, showIndex, players } = this.state;
 
-		if (gameStart) {
-			return <div>game</div>;
+		if (players.length <= 0) {
+			return <MyRedirect to="/setting" />
 		} else {
-			return people.length > 0 ? players.length > 0 ? (
-				<div className="App-header">
-					<h1 className="animated" ref="title">
-						이제 각 사람 마다의 역할이 정해집니다.
-					</h1>
+			return <div className="App-header">
+				<h1 className="animated" ref="title">
+					이제 각 사람 마다의 역할이 정해집니다.
+			</h1>
 
-					<h2>{players[showIndex].name}</h2>
-					{showRole && <h2 className="animated fadeInUp delay-.1s">{players[showIndex].jobName}</h2>}
-					{showRole && players[showIndex].jobName === JOB_NAME_OF_MAFIA &&
-						<>
-							<h3>동료 마피아</h3>
-							{players.filter((player) => players[showIndex].name !== player.name
-								&& player.jobName === JOB_NAME_OF_MAFIA)
-								.map((player, i) => (
-									<div key={`mafia-show-${i}`}>
-										{player.name}
-									</div>
-								))}
-						</>
-					}
+				<h2>{players[showIndex].name}</h2>
+				{showRole && <h2 className="animated fadeInUp delay-.1s">{players[showIndex].jobName}</h2>}
+				{showRole && players[showIndex].jobName === JOB_NAME_OF_MAFIA &&
+					<>
+						<h3>동료 마피아</h3>
+						{players.filter((player) => players[showIndex].name !== player.name
+							&& player.jobName === JOB_NAME_OF_MAFIA)
+							.map((player, i) => (
+								<div key={`mafia-show-${i}`}>
+									{player.name}
+								</div>
+							))}
+					</>
+				}
 
-					<button onClick={this.handleShowRole} className="btn">
-						다음
-					</button>
-				</div>
-			) : null : <MyRedirect to="/setting" />;
+				<button onClick={this.handleShowRole} className="btn">
+					다음
+			</button>
+			</div>
 		}
 	}
 }
 
 CheckRole.propTypes = {
-	players: PropTypes.array,
 	people: PropTypes.arrayOf(PropTypes.string),
 	jobs: PropTypes.arrayOf(
 		PropTypes.shape({
@@ -83,15 +82,13 @@ CheckRole.propTypes = {
 			jobName: PropTypes.string,
 			count: PropTypes.number
 		})
-	),
-	setRolePeople: PropTypes.func.isRequired
+	)
 };
 
 export default withRouter(
 	useGame(({ state, actions }) => ({
-		players: state.players,
 		people: state.people,
 		jobs: state.jobs,
-		setRolePeople: actions.setRolePeople
+		randomJobs: state.randomJobs
 	}))(CheckRole)
 );
