3cb3e0be1c293fed6b88fb38add47f542b91c89e
JeongHanYoung <8735132@gmail.com>
2019-03-06 11:15:04 +0900

    [18-03-06] Hooks > Game, DayTime, VoteTime까지 class component -> functional component

--- src/components/DayTime/VoteTime.js
+++ src/components/DayTime/VoteTime.js
@@ -1,70 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 
 
-class VoteTime extends React.Component {
-	constructor(props) {
-		super(props)
+export default function VoteTime({
+	votePerson, players, changeDayTimeOrder
+}) {
+	const [voteOrder, setVoteOrder] = useState(0);
 
-		this.state = {
-			voteOrder: 0
-		}
-	}
-	handleVote = (name) => {
-
-		const { voteOrder } = this.state;
-		const { votePerson, players, changeDayTimeOrder } = this.props
+	function _handleVote(name) {
 		votePerson(name)
 		if (players.size - 1 === voteOrder) {
-			this.setState({
-				voteOrder: 0
-			})
+			setVoteOrder(0)
 			changeDayTimeOrder()
 		} else {
-			this.setState({
-				voteOrder: voteOrder + 1
-			})
+			setVoteOrder(voteOrder + 1)
 		}
+	}
 
-	};
-	render() {
-		const { voteOrder } = this.state;
-		const { players } = this.props;
-
-		return (
-			<main className="daytime">
-				<h2 className="game-title" data-testid="game-title">hello mafia</h2>
-				<div className="game-content">
-					<p className="content-description" data-testid="game-content-description">마피아로 의심되는 사람을 투표합니다.</p>
-					<div className="voter">
-						<span className="voter-bold">
-							{players.getIn([voteOrder, 'name'])}
-						</span>
-						님의 투표</div>
-					<div className="vote-btn-container">
-						<div>
-							{players
-								.filter((person, i) => i !== voteOrder)
-								.map(person => (
-									<button
-										data-testid={`vote-button-${person.get('name')}`}
-										key={`vote-btn-${person.get('name')}`}
-										ref={`${person.get('name')}`}
-										onClick={() => this.handleVote(person.get('name'))}
-										className="btn-sm"
-									>
-										{person.get('name')}
-									</button>
-								))
-							}
-						</div>
+	return (
+		<main className="daytime">
+			<h2 className="game-title" data-testid="game-title">hello mafia</h2>
+			<div className="game-content">
+				<p className="content-description" data-testid="game-content-description">마피아로 의심되는 사람을 투표합니다.</p>
+				<div className="voter">
+					<span className="voter-bold">
+						{players.getIn([voteOrder, 'name'])}
+					</span>
+					님의 투표</div>
+				<div className="vote-btn-container">
+					<div>
+						{players
+							.filter((person, i) => i !== voteOrder)
+							.map(person => (
+								<button
+									data-testid={`vote-button-${person.get('name')}`}
+									key={`vote-btn-${person.get('name')}`}
+									onClick={() => _handleVote(person.get('name'))}
+									className="btn-sm"
+								>
+									{person.get('name')}
+								</button>
+							))
+						}
 					</div>
 				</div>
-			</main>
-		);
-	}
+			</div>
+		</main>
+	)
 }
+
+// class VoteTime extends React.Component {
+// 	constructor(props) {
+// 		super(props)
+
+// 		this.state = {
+// 			voteOrder: 0
+// 		}
+// 	}
+// 	handleVote = (name) => {
+
+// 		const { voteOrder } = this.state;
+// 		const { votePerson, players, changeDayTimeOrder } = this.props
+// 		votePerson(name)
+// 		if (players.size - 1 === voteOrder) {
+// 			this.setState({
+// 				voteOrder: 0
+// 			})
+// 			changeDayTimeOrder()
+// 		} else {
+// 			this.setState({
+// 				voteOrder: voteOrder + 1
+// 			})
+// 		}
+
+// 	};
+// 	render() {
+// 		const { voteOrder } = this.state;
+// 		const { players } = this.props;
+
+// 		return (
+// 			<main className="daytime">
+// 				<h2 className="game-title" data-testid="game-title">hello mafia</h2>
+// 				<div className="game-content">
+// 					<p className="content-description" data-testid="game-content-description">마피아로 의심되는 사람을 투표합니다.</p>
+// 					<div className="voter">
+// 						<span className="voter-bold">
+// 							{players.getIn([voteOrder, 'name'])}
+// 						</span>
+// 						님의 투표</div>
+// 					<div className="vote-btn-container">
+// 						<div>
+// 							{players
+// 								.filter((person, i) => i !== voteOrder)
+// 								.map(person => (
+// 									<button
+// 										data-testid={`vote-button-${person.get('name')}`}
+// 										key={`vote-btn-${person.get('name')}`}
+// 										ref={`${person.get('name')}`}
+// 										onClick={() => this.handleVote(person.get('name'))}
+// 										className="btn-sm"
+// 									>
+// 										{person.get('name')}
+// 									</button>
+// 								))
+// 							}
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</main>
+// 		);
+// 	}
+// }
 
 VoteTime.propTypes = {
 	votePerson: PropTypes.func.isRequired,
@@ -72,4 +119,4 @@ VoteTime.propTypes = {
 	changeDayTimeOrder: PropTypes.func.isRequired
 };
 
-export default VoteTime
+// export default VoteTime
