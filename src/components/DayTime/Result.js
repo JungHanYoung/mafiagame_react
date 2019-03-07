f16677188fa4ac24fbf2fb1cc24e0f84572c9435
JeongHanYoung <8735132@gmail.com>
2019-03-07 11:38:29 +0900

    [0307 - 함수형 컴포넌트] Game > DayTime > Result 수정 완

--- src/components/DayTime/Result.js
+++ src/components/DayTime/Result.js
@@ -3,105 +3,178 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import PropTypes from 'prop-types';
 import { JOB_NAME_OF_MAFIA } from '../../contants/Job';
 
-class Result extends React.Component {
+export default function Result({
+	players, changeDayTimeOrder, deletePlayer, moveRevote, moveToResult
+}) {
 
-	get isReVoted() {
+	const maxNumOfVotes = players.reduce((max, cur) => max.get('daytimeVoted') > cur.get('daytimeVoted') ? max : cur).get('daytimeVoted')
+	const maxOfVotesPlayers = players.filter(player => player.get('daytimeVoted') === maxNumOfVotes)
+	const isReVoted = maxOfVotesPlayers.size !== 1
 
-		const { players } = this.props
+	const votedPerson = players.reduce((max, cur) => max.get('daytimeVoted') < cur.get('daytimeVoted') ? cur : max)
+	//
 
-		const maxNumOfVotes = players.reduce((max, cur) => max.get('daytimeVoted') > cur.get('daytimeVoted') ? max : cur).get('daytimeVoted')
+	const after = players
+		.filter(player => player.get('name') !== votedPerson.get('name'))
+	const mafiaSize = after
+		.filter(player => player.get('jobName') === JOB_NAME_OF_MAFIA)
+		.size
+	const citizenSize = after
+		.filter(player => player.get('jobName') !== JOB_NAME_OF_MAFIA)
+		.size
+	const isVictory = mafiaSize >= citizenSize
+		? 'mafia' : mafiaSize === 0 ? 'citizen' : null;
 
-		const maxOfVotesPlayers = players.filter(player => player.get('daytimeVoted') === maxNumOfVotes)
+	//
 
-		return maxOfVotesPlayers.size !== 1
-	}
 
-	get votedPerson() {
-		const { players } = this.props;
-
-		return players.reduce((max, cur) => max.get('daytimeVoted') < cur.get('daytimeVoted') ? cur : max)
-	}
-
-	get isVictory() {
-		const { players } = this.props;
-
-		const after = players
-			.filter(player => player.get('name') !== this.votedPerson.get('name'))
-
-		// console.log(after);
-
-		const mafiaSize = after
-			.filter(player => player.get('jobName') === JOB_NAME_OF_MAFIA)
-			.size
-		const citizenSize = after
-			.filter(player => player.get('jobName') !== JOB_NAME_OF_MAFIA)
-			.size
-
-		if (mafiaSize >= citizenSize) {
-			return 'mafia'
-		} else if (mafiaSize === 0) {
-			return 'citizen'
-		} else {
-			return null
-		}
-	}
-
-	changeAtDay = () => {
-		const { changeDayTimeOrder, deletePlayer } = this.props;
-		const { isReVoted } = this
+	//
+	function changeAtDay() {
 		changeDayTimeOrder()
 		if (!isReVoted)
-			deletePlayer(this.votedPerson.get('name'))
+			deletePlayer(votedPerson.get('name'))
 	}
 
-	render() {
+	return (
+		<main className="sunset">
+			<h2 className="game-title">hello mafia</h2>
+			<div className="game-content">
+				<h2 className="subject" data-testid="content-subject">투표 결과</h2>
 
-		const { moveRevote, moveToResult } = this.props
-		const { isReVoted } = this
+				{isReVoted ? <p className="content-description" data-testid="content-description">투표가 동률이 났습니다.</p>
+					: <p className="content-description" data-testid="content-description"><span className="voted-person" data-testid="person">{votedPerson.get('name')}</span> 님이 죽으셨습니다.</p>}
 
-		// console.log(isVictory)
-
-		return (
-			<main className="sunset">
-				<h2 className="game-title">hello mafia</h2>
-				<div className="game-content">
-					<h2 className="subject" data-testid="content-subject">투표 결과</h2>
-
-					{isReVoted ? <p className="content-description" data-testid="content-description">투표가 동률이 났습니다.</p>
-						: <p className="content-description" data-testid="content-description"><span className="voted-person" data-testid="person">{this.votedPerson.get('name')}</span> 님이 죽으셨습니다.</p>}
-
-					{this.isVictory === 'mafia'
-						?
-						<>
-							<h3>마피아가 승리하였습니다.</h3>
+				{isVictory === 'mafia'
+					?
+					<>
+						<h3>마피아가 승리하였습니다.</h3>
+						<button
+							className="btn-lg"
+							onClick={moveToResult}>결과화면</button>
+					</>
+					: isVictory === 'citizen'
+						? <>
+							<h3>시민이 승리하였습니다.</h3>
 							<button
 								className="btn-lg"
 								onClick={moveToResult}>결과화면</button>
 						</>
-						: this.isVictory === 'citizen'
+						: isReVoted
 							? <>
-								<h3>시민이 승리하였습니다.</h3>
 								<button
 									className="btn-lg"
-									onClick={moveToResult}>결과화면</button>
-							</>
-							: isReVoted
-								? <>
-									<button
-										className="btn-lg"
-										onClick={moveRevote}>재투표</button>
-									<button
-										className="btn-lg"
-										onClick={this.changeAtDay}>밤이됩니다.</button>
-								</>
-								: <button
+									onClick={moveRevote}>재투표</button>
+								<button
 									className="btn-lg"
-									onClick={this.changeAtDay}>밤이 됩니다.</button>}
-				</div>
-			</main>
-		);
-	}
+									onClick={changeAtDay}>밤이됩니다.</button>
+							</>
+							: <button
+								className="btn-lg"
+								onClick={changeAtDay}>밤이 됩니다.</button>}
+			</div>
+		</main>
+	)
 }
+
+// class Result extends React.Component {
+
+// 	get isReVoted() {
+
+// 		const { players } = this.props
+
+// 		const maxNumOfVotes = players.reduce((max, cur) => max.get('daytimeVoted') > cur.get('daytimeVoted') ? max : cur).get('daytimeVoted')
+
+// 		const maxOfVotesPlayers = players.filter(player => player.get('daytimeVoted') === maxNumOfVotes)
+
+// 		return maxOfVotesPlayers.size !== 1
+// 	}
+
+// 	get votedPerson() {
+// 		const { players } = this.props;
+
+// 		return players.reduce((max, cur) => max.get('daytimeVoted') < cur.get('daytimeVoted') ? cur : max)
+// 	}
+
+// 	get isVictory() {
+// 		const { players } = this.props;
+
+// 		const after = players
+// 			.filter(player => player.get('name') !== this.votedPerson.get('name'))
+
+// 		// console.log(after);
+
+// 		const mafiaSize = after
+// 			.filter(player => player.get('jobName') === JOB_NAME_OF_MAFIA)
+// 			.size
+// 		const citizenSize = after
+// 			.filter(player => player.get('jobName') !== JOB_NAME_OF_MAFIA)
+// 			.size
+
+// 		if (mafiaSize >= citizenSize) {
+// 			return 'mafia'
+// 		} else if (mafiaSize === 0) {
+// 			return 'citizen'
+// 		} else {
+// 			return null
+// 		}
+// 	}
+
+// 	changeAtDay = () => {
+// 		const { changeDayTimeOrder, deletePlayer } = this.props;
+// 		const { isReVoted } = this
+// 		changeDayTimeOrder()
+// 		if (!isReVoted)
+// 			deletePlayer(this.votedPerson.get('name'))
+// 	}
+
+// 	render() {
+
+// 		const { moveRevote, moveToResult } = this.props
+// 		const { isReVoted } = this
+
+// 		// console.log(isVictory)
+
+// 		return (
+// 			<main className="sunset">
+// 				<h2 className="game-title">hello mafia</h2>
+// 				<div className="game-content">
+// 					<h2 className="subject" data-testid="content-subject">투표 결과</h2>
+
+// 					{isReVoted ? <p className="content-description" data-testid="content-description">투표가 동률이 났습니다.</p>
+// 						: <p className="content-description" data-testid="content-description"><span className="voted-person" data-testid="person">{this.votedPerson.get('name')}</span> 님이 죽으셨습니다.</p>}
+
+// 					{this.isVictory === 'mafia'
+// 						?
+// 						<>
+// 							<h3>마피아가 승리하였습니다.</h3>
+// 							<button
+// 								className="btn-lg"
+// 								onClick={moveToResult}>결과화면</button>
+// 						</>
+// 						: this.isVictory === 'citizen'
+// 							? <>
+// 								<h3>시민이 승리하였습니다.</h3>
+// 								<button
+// 									className="btn-lg"
+// 									onClick={moveToResult}>결과화면</button>
+// 							</>
+// 							: isReVoted
+// 								? <>
+// 									<button
+// 										className="btn-lg"
+// 										onClick={moveRevote}>재투표</button>
+// 									<button
+// 										className="btn-lg"
+// 										onClick={this.changeAtDay}>밤이됩니다.</button>
+// 								</>
+// 								: <button
+// 									className="btn-lg"
+// 									onClick={this.changeAtDay}>밤이 됩니다.</button>}
+// 				</div>
+// 			</main>
+// 		);
+// 	}
+// }
 
 Result.propTypes = {
 	players: ImmutablePropTypes.list,
@@ -111,4 +184,4 @@ Result.propTypes = {
 	moveRevote: PropTypes.func.isRequired
 };
 
-export default Result
+// export default Result
