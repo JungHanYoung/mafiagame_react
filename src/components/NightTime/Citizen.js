68356299f2d27f38f41c839c4a010017104ed965
JeongHanYoung <8735132@gmail.com>
2019-03-07 12:20:08 +0900

    NightTime>Citizen.js 컴포넌트 수정 와뇨

--- src/components/NightTime/Citizen.js
+++ src/components/NightTime/Citizen.js
@@ -2,29 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Layout from './Layout';
 
-class Citizen extends React.Component {
-	handleSelectBtn = () => {
-		const { toggleConfirmed, changeNightTimeOrder } = this.props;
+export default function Citizen({
+	toggleConfirmed, changeNightTimeOrder
+}) {
+	function handleSelectBtn() {
 		toggleConfirmed()
 		changeNightTimeOrder()
-	};
-	render() {
-		return (
-			<>
-				<Layout describe={`당신은 시민입니다.`} />
-				<button
-					className="btn-lg"
-					onClick={this.handleSelectBtn}>
-					다음
-					</button>
-			</>
-		);
 	}
+
+	return <>
+		<Layout describe={`당신은 시민입니다.`} />
+		<button
+			className="btn-lg"
+			onClick={handleSelectBtn}>
+			다음
+			</button>
+	</>
 }
+
+// class Citizen extends React.Component {
+// 	handleSelectBtn = () => {
+// 		const { toggleConfirmed, changeNightTimeOrder } = this.props;
+// 		toggleConfirmed()
+// 		changeNightTimeOrder()
+// 	};
+// 	render() {
+// 		return (
+// 			<>
+// 				<Layout describe={`당신은 시민입니다.`} />
+// 				<button
+// 					className="btn-lg"
+// 					onClick={this.handleSelectBtn}>
+// 					다음
+// 					</button>
+// 			</>
+// 		);
+// 	}
+// }
 
 Citizen.propTypes = {
 	toggleConfirmed: PropTypes.func.isRequired,
 	changeNightTimeOrder: PropTypes.func.isRequired
 };
 
-export default Citizen
+// export default Citizen
