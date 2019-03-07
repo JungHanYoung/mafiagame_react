e40ebcd0df05514644a1a85b4b90f2a86a6b1568
JeongHanYoung <8735132@gmail.com>
2019-03-07 12:09:09 +0900

    NightTime>Mafia 컴포넌트 수정 완

--- src/components/NightTime/Mafia.js
+++ src/components/NightTime/Mafia.js
@@ -4,46 +4,85 @@ import ImmutablePropTypes from 'react-immutable-proptypes'
 import { JOB_NAME_OF_MAFIA } from '../../contants/Job';
 import Layout from './Layout';
 
-
-class Mafia extends React.Component {
-	handleSelectBtn = (name) => {
-		const { toggleConfirmed, handleVote, changeNightTimeOrder } = this.props;
+export default function Mafia({
+	players, mafiaVotes, handleVote, toggleConfirmed, changeNightTimeOrder
+}) {
+	function handleSelectBtn(name) {
 		toggleConfirmed()
 		handleVote(name)
 		changeNightTimeOrder()
-	};
-	render() {
-		const { players, mafiaVotes } = this.props;
-		const mafias = Object.keys(mafiaVotes.toJS()).filter(mafia => mafiaVotes.get(mafia))
-		return (
-			<Layout
-				describe={`당신은 마피아 입니다.
-			죽일 사람을 선택하십시오.`}>
-				{mafias.length > 0 && (
-					<>
-						<div>마피아 투표 현황</div>
-						{mafias.map(mafia => (
-							<div key={`mafia-vote-co-${mafia}`}>
-								<span>{mafia}: </span><span>{mafiaVotes.get(mafia)}</span>
-							</div>
-						))}
-					</>
-				)}
-				<div className="vote-btn-container">
-					<div>
-						{players
-							.filter((person) => person.get('jobName') !== JOB_NAME_OF_MAFIA)
-							.map((person, i) => (
-								<button className="btn-sm" key={`mafia-select-${i}`} onClick={() => this.handleSelectBtn(person.get('name'))}>
-									{person.get('name')}
-								</button>
-							))}
-					</div>
-				</div>
-			</Layout>
-		);
 	}
+
+	const mafias = Object.keys(mafiaVotes.toJS()).filter(mafia => mafiaVotes.get(mafia))
+
+	return (
+		<Layout
+			describe={`당신은 마피아 입니다.
+			죽일 사람을 선택하십시오.`}>
+			{mafias.length > 0 && (
+				<>
+					<div>마피아 투표 현황</div>
+					{mafias.map(mafia => (
+						<div key={`mafia-vote-co-${mafia}`}>
+							<span>{mafia}: </span><span>{mafiaVotes.get(mafia)}</span>
+						</div>
+					))}
+				</>
+			)}
+			<div className="vote-btn-container">
+				<div>
+					{players
+						.filter((person) => person.get('jobName') !== JOB_NAME_OF_MAFIA)
+						.map((person, i) => (
+							<button className="btn-sm" key={`mafia-select-${i}`} onClick={() => handleSelectBtn(person.get('name'))}>
+								{person.get('name')}
+							</button>
+						))}
+				</div>
+			</div>
+		</Layout>
+	)
 }
+
+// class Mafia extends React.Component {
+// 	handleSelectBtn = (name) => {
+// 		const { toggleConfirmed, handleVote, changeNightTimeOrder } = this.props;
+// 		toggleConfirmed()
+// 		handleVote(name)
+// 		changeNightTimeOrder()
+// 	};
+// 	render() {
+// 		const { players, mafiaVotes } = this.props;
+// 		const mafias = Object.keys(mafiaVotes.toJS()).filter(mafia => mafiaVotes.get(mafia))
+// 		return (
+// 			<Layout
+// 				describe={`당신은 마피아 입니다.
+// 			죽일 사람을 선택하십시오.`}>
+// 				{mafias.length > 0 && (
+// 					<>
+// 						<div>마피아 투표 현황</div>
+// 						{mafias.map(mafia => (
+// 							<div key={`mafia-vote-co-${mafia}`}>
+// 								<span>{mafia}: </span><span>{mafiaVotes.get(mafia)}</span>
+// 							</div>
+// 						))}
+// 					</>
+// 				)}
+// 				<div className="vote-btn-container">
+// 					<div>
+// 						{players
+// 							.filter((person) => person.get('jobName') !== JOB_NAME_OF_MAFIA)
+// 							.map((person, i) => (
+// 								<button className="btn-sm" key={`mafia-select-${i}`} onClick={() => this.handleSelectBtn(person.get('name'))}>
+// 									{person.get('name')}
+// 								</button>
+// 							))}
+// 					</div>
+// 				</div>
+// 			</Layout>
+// 		);
+// 	}
+// }
 
 Mafia.propTypes = {
 	players: ImmutablePropTypes.list,
@@ -53,4 +92,4 @@ Mafia.propTypes = {
 	changeNightTimeOrder: PropTypes.func.isRequired
 };
 
-export default Mafia
+// export default Mafia
